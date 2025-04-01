ee783ee9851d9fc6e4de8625d76fc0227e926d4f
Kush Patel <pkush74@gmail.com>
2025-04-01 17:06:52 -0500

    init

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,15 +5,17 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyABDvNF-vICO7eLon3BNQsPbmrp4O8bj5Q",
-  authDomain: "icancookai.com/auth",
+  authDomain: "icancookauth.firebaseapp.com",
   projectId: "icancookauth",
   storageBucket: "icancookauth.firebasestorage.app",
   messagingSenderId: "831651857823",
   appId: "1:831651857823:web:96e76943fab48eb1a969aa",
   measurementId: "G-M1KC8ZVEKS"
 };
+
 
 // Initialize Firebase only if it hasn't been initialized already
 let app;
