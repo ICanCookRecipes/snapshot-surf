6d541e14bf9dc1549b56a00eb4d2000d710e7045
Kush Patel <pkush74@gmail.com>
2025-04-01 16:43:43 -0500

    init

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBXeZA_E4lO4UOBDEOMlPIMRpCFZcZIvTw",
-  authDomain: "icancook-app.firebaseapp.com",
-  projectId: "icancook-app",
-  storageBucket: "icancook-app.appspot.com",
-  messagingSenderId: "395929896312",
-  appId: "1:395929896312:web:95e9f448581090d0c0d6b3",
-  measurementId: "G-219DMP2SMT"
+  apiKey: "AIzaSyABDvNF-vICO7eLon3BNQsPbmrp4O8bj5Q",
+  authDomain: "icancookauth.firebaseapp.com",
+  projectId: "icancookauth",
+  storageBucket: "icancookauth.firebasestorage.app",
+  messagingSenderId: "831651857823",
+  appId: "1:831651857823:web:96e76943fab48eb1a969aa",
+  measurementId: "G-M1KC8ZVEKS"
 };
 
 // Initialize Firebase
